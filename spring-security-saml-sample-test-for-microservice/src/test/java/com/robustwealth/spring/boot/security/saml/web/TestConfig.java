34b63b55e710207dcc25ba4f7c7002a343554d7b
Ming <mingli19850915@gmail.com>
2017-12-29 16:13:37 -0500

    Init spring-security-saml2 for microservice using springboot

---
+++ spring-security-saml-sample-test-for-microservice/src/test/java/com/robustwealth/spring/boot/security/saml/web/TestConfig.java
@@ -0,0 +1,8 @@
+package com.robustwealth.spring.boot.security.saml.web;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class TestConfig extends WebMvcConfigurerAdapter {
+}
